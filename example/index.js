287b40b20289e5ae0b0ff5048e740e1f89fd2ad8
Dora Czinege <czinege.dodo@gmail.com>
2022-07-19 19:53:27 +0200

    bombPlanted check

--- example/index.js
+++ example/index.js
@@ -1,5 +1,19 @@
 const fs = require("fs");
+const { off } = require("process");
 const CSGOGSI = require("../index"); // const CSGOGSI = require("node-csgo-gsi");
+
+/*enum states{
+    off,
+    freezetime,
+    live,
+    bombActive,
+    bombExploded,
+    bombDefused,
+    ctWin,
+    tWin
+}
+states gameState = off;*/
+
 
 let gsi = new CSGOGSI({
     port: 3000,
@@ -10,16 +24,45 @@ gsi.on("all", function (data) {
     fs.appendFileSync("./payload.txt", JSON.stringify(data, null, 2));
 });
 
-gsi.on("gameMap", (map) => console.log(`gameMap: ${map}`));
+//gsi.on("gameMap", (map) => console.log(`gameMap: ${map}`));
 gsi.on("gamePhase", (phase) => console.log(`gamePhase: ${phase}`));
-gsi.on("gameRounds", (rounds) => console.log(`gameRounds: ${rounds}`));
-gsi.on("gameCTscore", (score) => console.log(`gameCTscore: ${score}`));
-gsi.on("gameTscore", (score) => console.log(`gameTscore: ${score}`));
-gsi.on("roundWins", (wins) => console.log(`roundWins: ${roundWins}`));
-gsi.on("player", (player) => console.log(`player: ${player}`));
-gsi.on("roundPhase", (phase) => console.log(`roundPhase: ${phase}`));
-gsi.on("roundWinTeam", (team) => console.log(`roundWinTeam HUAT AH: ${team}`));
-gsi.on("bombState", (state) => console.log(`bombState: ${state}`));
-gsi.on("bombTimeStart", (time) => console.log(`bombTimeStart: ${time}`));
-gsi.on("bombExploded", () => console.log(`bombExploded`));
-gsi.on("bombTimeLeft", (time) => console.log(`bombTimeLeft: ${time}`));
+//gsi.on("gameRounds", (rounds) => console.log(`gameRounds: ${rounds}`));
+gsi.on("gameCTscore", (team_ct) => console.log(`gameCTscore: ${team_ct}`));
+gsi.on("gameTscore", (team_t) => console.log(`gameTscore: ${team_t}`));
+//gsi.on("roundWins", (wins) => console.log(`roundWins: ${roundWins}`));
+//gsi.on("player", (player) => console.log(`player: ${player}`));
+//gsi.on("roundPhase", (phase) => console.log(`roundPhase: ${phase}`));
+//gsi.on("roundWinTeam", (team) => console.log(`roundWinTeam HUAT AH: ${team}`));
+//gsi.on("bombState", (state) => console.log(`bombState: ${state}`));
+//gsi.on("bombTimeStart", (time) => console.log(`bombTimeStart: ${time}`));
+//gsi.on("bombExploded", () => console.log(`bombExploded`));
+//gsi.on("bombTimeLeft", (time) => console.log(`bombTimeLeft: ${time}`));
+
+//gsi.on("phaseCountdowns", (phase_countdowns) => console.log(`gameTscore: ${team_t}`));
+
+//Sending midi signal when bomb is planted
+var bombPlanted = false;
+gsi.on("bombState", function(state){
+    if(state=="planted" && !bombPlanted){
+        bombPlanted = true;
+        console.log("Midi signal: bomb planted");
+    }
+});
+
+//Sending midi signal when round is over
+//Problem: when a new game is starting: roundphase: over, over, freezetime, live 
+//Rather: check which team wins: T, CT, if no teams won yet: undefined
+//roundWinTeam seems to be staying the winner team, it will not go back to undefined 
+var roundOver = false;
+gsi.on("roundWinTeam", function(team){
+    if((team=="T" || team=="CT") && !roundOver){
+        bombPlanted = false;
+        roundOver = true; 
+        console.log("Midi signal: round over, reset lightning");
+    }
+    else if(typeof team == 'undefined'){
+        roundOver = false;
+    }
+}); 
+
+//round start
--- index.js
+++ index.js
@@ -54,8 +54,8 @@ class CSGOGSI extends EventEmitter {
             this.emit("gameMap", data["map"]["name"]);
             this.emit("gamePhase", data["map"]["phase"]); //warmup etc
             this.emit("gameRounds", data["map"]["round"]);
-            this.emit("gameCTscore", data["map"]["team_ct"]);
-            this.emit("gameTscore", data["map"]["team_t"]);
+            this.emit("gameCTscore", data["map"]["team_ct"]["score"]);
+            this.emit("gameTscore", data["map"]["team_t"]["score"]);
         }
 
         if (data["round_wins"]) {
@@ -65,6 +65,10 @@ class CSGOGSI extends EventEmitter {
         if (data["player"]) {
             this.emit("player", data["player"]);
         }
+
+        /*if (data["phase_countdowns"]) {
+            this.emit("phaseCountdowns", data["phase_countdowns"]);
+        }*/
 
         if (data["round"]) {
             this.emit("roundPhase", data["round"]["phase"]);
